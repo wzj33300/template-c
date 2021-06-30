c1ea2fffe72293cd16efd6f8a533827e696fdb44
wzj33300 <wzj33300@outlook.com>
2021-06-30 14:34:51 +0800

    新增内容

--- 数据结构/栈/栈.hpp
+++ 数据结构/栈/栈.hpp
@@ -1,5 +1,50 @@
 #include <iostream>
-namespace my_stack {
-    struct stack {
-        };
-}  // namespace my_stack
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+namespace MyStack {
+	template<class T>
+	struct Stack {
+		int maxSize;
+		T *a;
+		int len;
+
+		Stack(int maxn) {
+			maxSize = maxn;
+			a = new T[maxSize];
+			len = 0;
+		}
+
+		~Stack() {
+			delete a;
+		}
+
+		void pop() {
+			if (empty()) {
+				throw "stack is empty!!!";
+			}
+			--len;
+		}
+
+		void push(T data) {
+			if (len == maxSize) {
+				throw "stack is full!!!";
+			}
+			a[len++] = data;
+		}
+
+		T &top() {
+			return a[len - 1];
+		}
+
+		int size() {
+			return len;
+		}
+
+		bool empty() {
+			return len;
+		}
+	};
+}
